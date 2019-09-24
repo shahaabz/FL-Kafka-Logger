69ed0a8659a477699e15ab62142102bdc9249923
Syed Shahaab <fle@Syed-Shahaabs-MacBook-Pro.local>
2019-09-24 19:11:02 +0530

    kafka added json body

--- FL-KAFKA-LOGGER/index.js
+++ FL-KAFKA-LOGGER/index.js
@@ -1,31 +1,62 @@
 const kafka = require('kafka-node');
-exports.logFlData = (kafkaConfig, data) => {
-    return new Promise((resolve, reject) => {
-        if (kafkaConfig == undefined) return reject("Undefined Kafka config.");
-        if (kafkaConfig.host == undefined) return reject("Undefined kafka host.");
-        if (kafkaConfig.topic == undefined) return reject("Undefined kafka topic.");
-        if (kafkaConfig.partition == undefined) kafkaConfig.partition = 0;
-        const client = new kafka.KafkaClient({ kafkaHost: kafkaConfig.host });
-        let Producer = kafka.Producer;
-        // let KeyedMessage = kafka.KeyedMessage;
-        let producer = new Producer(client);
-        // let km = new KeyedMessage('key', 'message');
-        let payloads = [{ topic: kafkaConfig.topic, messages: data, partition: kafkaConfig.partition }];
-        try {
-            producer.on('ready', () => {
-                producer.send(payloads, (error, data) => {
-                    if (error) {
-                        throw new Error(error);
-                    }
-                    let response = { success: true, data: data }
-                    return resolve(response);
+
+class FlKafkaLogger {
+
+    constructor(kafkaConfig) {
+        if (!kafkaConfig) throw new Error("Undefined Kafka config.");
+        if (!kafkaConfig.host) throw new Error("Undefined kafka host.");
+        if (!kafkaConfig.topic) throw new Error("Undefined kafka topic.");
+
+        this.topic = kafkaConfig.topic;
+        this.partition = !kafkaConfig.partition ? 0 : kafkaConfig.partition;
+        this.client = new kafka.KafkaClient({ kafkaHost: kafkaConfig.host });
+        this.Producer = kafka.Producer;
+        this.producer = new Producer(client);
+    }
+
+    /**
+     * @description Send data to kafka 
+     * @param {Number} statusCode
+     * @param {String} method
+     * @param {String} appName
+     * @param {JSON} data
+     * @memberof FlKafkaLogger
+     */
+    logFlData = (statusCode, methodName, appName, data) => {
+        return new Promise((resolve, reject) => {
+            statusCode = statusCode ? statusCode : '';
+
+            if (!methodName) return reject("undefined method name.");
+            if (!appName) return reject("undefined application name.");
+            if (!data) return reject("undefined data object.");
+
+            let logData = {
+                statusCode: statusCode,
+                methodName: methodName,
+                appName: appName,
+                source: this.topic,
+                metaData: data
+            }
+
+            let payloads = [{ topic: this.topic, messages: logData, partition: this.partition }];
+            try {
+                this.producer.on('ready', () => {
+                    producer.send(payloads, (error, data) => {
+                        if (error) {
+                            return reject(error);
+                        }
+                        let response = { success: true, data: data }
+                        return resolve(response);
+                    });
                 });
-            });
-            producer.on('error', (error) => {
+                this.producer.on('error', (error) => {
+                    return reject(error);
+                });
+            } catch (error) {
                 return reject(error);
-            });
-        } catch (error) {
-            return reject(error);
-        }
-    });
+            }
+        });
+    }
 }
+
+module.exports = FlKafkaLogger();
